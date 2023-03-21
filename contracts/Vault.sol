26523fe2bed8211b25fb8646b0cd784655262dca
cryptofrogonline <126547998+cryptofrogonline@users.noreply.github.com>
2023-03-21 16:19:50 +0800

    Update Vault.sol

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
